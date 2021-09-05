d74f5ed0ef4402f6c7f3ca4c3aab9a1e1aa62aae
NagisaCo <yjr888@vip.qq.com>
2021-09-05 13:18:13 +0800

    fix rss bug

--- src/plugins/video_down/rss/__init__.py
+++ src/plugins/video_down/rss/__init__.py
@@ -4,15 +4,21 @@
 # @Time     : 2021/9/4 21:02
 # @Author   : NagisaCo
 import nonebot
-from nonebot import require
+from nonebot import require, get_driver
 from apscheduler.triggers.interval import IntervalTrigger
 from .data_source import YouRss
+from .config import Config
+
+global_config = get_driver().config
+status_config = Config(**global_config.dict())
+
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
+video_get = nonebot.plugin.require('src.plugins.video_down')
 
 
 @scheduler.scheduled_job(IntervalTrigger(minutes=10))
 async def update():
-    you = YouRss(["UCXuqSBlHAE6Xw-yeJA0Tunw", "UCLxAS02eWvfZK4icRNzWD_g"], 'http://127.0.0.1:7890')
+    you = YouRss(status_config.you_rss_channel_list, video_get.check_proxy())
     await you.update()
 
