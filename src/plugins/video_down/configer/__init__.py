c2cdeaa989355e6cd2aa31ada07c6566ab594441
NagisaCo <yjr888@vip.qq.com>
2021-12-10 22:47:59 +0800

    fix(configer): 修复群号int转str

--- src/plugins/video_down/configer/__init__.py
+++ src/plugins/video_down/configer/__init__.py
@@ -1,5 +1,5 @@
 import os
-from nonebot import get_driver
+from nonebot import get_driver, plugin
 
 from .config import Config
 from .data_source import Configer
@@ -7,5 +7,8 @@ from .data_source import Configer
 global_config = get_driver().config
 status_config = Config(**global_config.dict())  # 获取nonebot配置
 
+export = plugin.export()  # 导出插件
 
 configer = Configer(status_config.video_config_file)
+
+export.configer = configer
--- src/plugins/video_down/configer/data_source.py
+++ src/plugins/video_down/configer/data_source.py
@@ -26,6 +26,8 @@ class Configer:
     """
 
     INIT_CONFIG = {
+        "group_id": 0,
+        "group_name": "",
         "video_upload_path": "",
         "cover_upload_path": "",
         "enable_subscribe": False,
@@ -35,8 +37,9 @@ class Configer:
     def __init__(self, config_path):
         self.config_path = config_path
         self.config_data = None
+        self.read_config_file()
 
-    async def read_config_file(self):
+    def read_config_file(self):
         """
         读取配置文件
         """
@@ -48,11 +51,11 @@ class Configer:
         if not os.path.exists(self.config_path):
             logger.warning(f'配置文件不存在，创建默认配置文件：{self.config_path}')
             try:
-                if not os.path.exists(os.path.dirname(self.config_path)):  # 文件夹不存在
+                if not os.path.exists(os.path.dirname(os.path.abspath(self.config_path))):  # 文件夹不存在
                     os.makedirs(os.path.dirname(self.config_path))  # 创建文件夹
 
-                async with aiofiles.open(self.config_path, 'w', encoding='utf-8') as f:
-                    await f.write(json.dumps({}, ensure_ascii=False, indent=4))  # 创建空配置文件
+                with open(self.config_path, 'w', encoding='utf-8') as f:
+                    f.write(json.dumps({}, ensure_ascii=False, indent=4))  # 创建空配置文件
 
             except Exception as e:  # 创建失败
                 logger.error(f'创建配置文件{self.config_path}失败：{e}')
@@ -62,8 +65,8 @@ class Configer:
 
         # 读取文件
         try:
-            async with aiofiles.open(self.config_path, 'r', encoding='utf-8') as f:
-                self.config_data = json.loads(await f.read())  # 读取配置文件
+            with open(self.config_path, 'r', encoding='utf-8') as f:
+                self.config_data = json.loads(f.read())  # 读取配置文件
                 logger.debug(f'读取配置文件：{self.config_path}')
 
         except Exception as e:  # 读取失败
@@ -97,7 +100,7 @@ class Configer:
         """
         return group_id in self.config_data
 
-    def get_config_data(self, group_id: int = None) -> dict:
+    def get_config_data(self, group_id=None) -> dict:
         """
         获取配置文件内容
 
@@ -107,16 +110,20 @@ class Configer:
         """
         if group_id is None:  # 获取全部配置
             return self.config_data
+        if type(group_id) == int:  # 字符串化
+            group_id = str(group_id)
         if group_id in self.config_data:  # 获取指定配置
             return self.config_data[group_id]
         return {}  # 没有配置
 
-    def set_config_data(self, group_id: int, video_upload_path: str = None, cover_upload_path: str = None,
-                        enable_subscribe: bool = None) -> None:
+    async def set_config_data(self, group_id, group_name: str = None, video_upload_path: str = None,
+                              cover_upload_path: str = None, enable_subscribe: bool = None) -> None:
         """
         设置配置信息
 
         :param group_id: 群号
+
+        :param group_name: 群名
 
         :param video_upload_path: 视频上传路径
 
@@ -124,10 +131,16 @@ class Configer:
 
         :param enable_subscribe: 是否开启订阅
         """
+        if type(group_id) == int:  # 字符串化
+            group_id = str(group_id)
+
         if group_id not in self.config_data:  # 不存在该群配置
             self.config_data[group_id] = Configer.INIT_CONFIG
+            self.config_data[group_id]['group_id'] = group_id
 
         # 更新配置
+        if group_name:
+            self.config_data[group_id]['group_name'] = group_name
         if video_upload_path:
             self.config_data[group_id]['video_upload_path'] = video_upload_path
         if cover_upload_path:
@@ -135,7 +148,22 @@ class Configer:
         if enable_subscribe:
             self.config_data[group_id]['enable_subscribe'] = enable_subscribe
 
-    def get_subscribe_channel(self, group_id: int):
+        await self.save_config_file()
+
+    def delete_config_data(self, group_id):
+        """
+        删除配置
+
+        :param group_id: 群号
+        """
+        if type(group_id) == int:  # 字符串化
+            group_id = str(group_id)
+
+        if group_id in self.config_data:
+            self.config_data.pop(group_id)
+            self.save_config_file()
+
+    def get_subscribe_channel(self, group_id):
         """
         获取订阅列表
 
@@ -143,6 +171,9 @@ class Configer:
 
         :return: 列表引用
         """
+        if type(group_id) == int:  # 字符串化
+            group_id = str(group_id)
+
         if group_id in self.config_data:  # 获取指定配置
             return self.config_data[group_id]['subscribe_channel']  # 返回引用
         return None
